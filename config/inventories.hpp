007492401215b6ea3c7f6978e6bc90931b816af6
C222 <c222streams@gmail.com>
2016-11-02 17:39:04 -0700

    Refining available inventories

--- config/inventories.hpp
+++ config/inventories.hpp
@@ -19,14 +19,75 @@ class WEST4
 	vehicle = "B_soldier_AR_F"
 };
 
+class WEST5
+{
+	vehicle = "B_soldier_M_F"
+};
+
+class WEST6
+{
+	vehicle = "B_medic_F"
+};
+
 //EAST
 class EAST1
 {
-	vahicle = "O_G_Soldier_F"
+	vehicle = "O_Soldier_F"
 };
+
+class EAST2
+{
+	vehicle = "O_Soldier_lite_F"
+};
+
+class EAST3
+{
+	vehicle = "O_Soldier_GL_F"
+};
+
+class EAST4
+{
+	vehicle = "O_Soldier_AR_F"
+};
+
+class EAST5
+{
+	vehicle = "O_soldier_M_F"
+};
+
+class EAST6
+{
+	vehicle = "O_medic_F"
+};
+
 
 //GUER
 class GUER1
 {
-	vahicle = "I_G_Soldier_F"
+	vehicle = "I_soldier_F"
+};
+
+class GUER2
+{
+	vehicle = "I_Soldier_lite_F"
+};
+
+class GUER3
+{
+	vehicle = "I_Soldier_GL_F"
+};
+
+class GUER4
+{
+	vehicle = "I_Soldier_AR_F"
+};
+
+class GUER5
+{
+	vehicle = "I_Soldier_M_F"
+};
+
+class GUER6
+{
+	vehicle = "I_medic_F"
 };
--- server/addInventories.sqf
+++ server/addInventories.sqf
@@ -1,8 +1,8 @@
 private ["_west_inventories", "_east_inventories", "_guer_inventories"];
 
-_west_inventories = ["WEST1", "WEST2", "WEST3", "WEST4"];
-_east_inventories = ["EAST1"];
-_guer_inventories = ["GUER1"];
+_west_inventories = ["WEST1", "WEST2", "WEST3", "WEST4", "WEST5", "WEST6"];
+_east_inventories = ["EAST1", "EAST2", "EAST3", "EAST4", "EAST5", "EAST6"];
+_guer_inventories = ["GUER1", "GUER2", "GUER3", "GUER4", "GUER5", "GUER6"];
 
 {
 	[west, _x] call BIS_fnc_addRespawnInventory;
